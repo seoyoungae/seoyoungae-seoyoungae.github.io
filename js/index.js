5c67b804a26a9382d3a57b4a27710fe9bd178ca7
seoyoungae <yy034@naver.com>
2023-03-23 17:54:03 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 let i, num=0, q, a=0, result=0;
 
@@ -21,4 +22,29 @@ window.onload = function(){
         }
        
     })
+=======
+
+let i, num=0, q, a=0, result=0;
+
+window.onload = function(){
+    $(".test1").on("click", function(){
+        location.href="/test1/index.html";
+    })
+
+    $(".test2").on("click", function(){
+        location.href="/test2/index.html";
+    })
+
+    $(".tag_box>li").on("click", function(){
+        i=$(this).index()+1;
+        console.log(i);
+        if(i===1){
+            $(".test_list>div").show();
+        }else{
+            $(".test_list>div").hide();
+            $(".tag_"+i).show();
+        }
+       
+    })
+>>>>>>> eda0291c7b7c346f69ddb9f14a7042b76fce38c2
 }
