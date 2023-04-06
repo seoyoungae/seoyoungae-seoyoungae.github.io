cc04a331a230f0d56bb8433cb69389ac876cb721
seoyoungae <yy034@naver.com>
2023-04-06 18:11:34 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
-
-let i, num=0, q, a=0, result=0;
+let i, num=0, q, a=0, result=0, connectedDate,prev_time=0,time=0;
 
 window.onload = function(){
+    connectedDate = new Date();
+    console.log("시작 시간 :"+ connectedDate);
+    console.log(prev_time);
+    
+    if(window.name){
+        prev_time = window.name;
+    }
+    
+    window.name=prev_time;
+    setInterval(function(){
+        time++;
+        console.log(time);
+        window.name=time;
+        prev_tiem=time;
+    },1000)
+    // setInterval(function(){
+    //     var currentDate = new Date();
+    //     console.log(currentDate);
+    //     time = Number((currentDate - connectedDate)/1000);
+    //     console.log(time);
+    //     window.name = time;
+        
+    // },1000)
     $(".test1").on("click", function(){
         location.href="/test1/index.html";
     })
@@ -22,3 +44,25 @@ window.onload = function(){
         }
     })
 }
+
+function once(fn, context) { 
+    var result;
+ 
+    return function() { 
+        if(fn) {
+            result = fn.apply(context || this, arguments);
+            fn = null;
+        }
+ 
+        return result;
+    };
+}
+ 
+// Usage
+var canOnlyFireOnce = once(function() {
+    prev_time=1800;
+});
+ 
+
+  
+  
